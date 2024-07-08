702a37f77c025b21dd2b045c5492ef3417d66818
Benjamin Muschko <benjamin.muschko@gmail.com>
2024-07-08 09:45:02 -0600

    Fix removed Kubernetes legacy repository

--- app-a/ch02/backup-restore-etcd/control-plane.sh
+++ app-a/ch02/backup-restore-etcd/control-plane.sh
@@ -5,18 +5,20 @@ API_ADV_ADDRESS=$2
 
 kubeadm init --pod-network-cidr $POD_CIDR --apiserver-advertise-address $API_ADV_ADDRESS | tee /vagrant/kubeadm-init.out
 
+systemctl daemon-reload
+echo "KUBELET_EXTRA_ARGS=--node-ip=$API_ADV_ADDRESS --cgroup-driver=systemd" > /etc/default/kubelet
+systemctl restart kubelet
+
 mkdir -p /home/vagrant/.kube
 cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown vagrant:vagrant /home/vagrant/.kube/config
 mkdir -p /root/.kube
 cp -i /etc/kubernetes/admin.conf /root/.kube/config
 
-kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/tigera-operator.yaml
+wget https://raw.githubusercontent.com/projectcalico/calico/v3.27.2/manifests/custom-resources.yaml
+sed -i 's~cidr: 192\.168\.0\.0/16~cidr: 172\.18\.0\.0/16~g' custom-resources.yaml
+kubectl create -f custom-resources.yaml
+rm custom-resources.yaml
 
 cp /etc/kubernetes/admin.conf /vagrant/admin.conf
-
-# Install etcdctl
-ETCD_VERSION="v3.4.14"
-wget https://github.com/etcd-io/etcd/releases/download/$ETCD_VERSION/etcd-$ETCD_VERSION-linux-amd64.tar.gz -O /tmp/etcd-$ETCD_VERSION-linux-amd64.tar.gz
-tar xvf /tmp/etcd-$ETCD_VERSION-linux-amd64.tar.gz -C /tmp
-sudo mv /tmp/etcd-$ETCD_VERSION-linux-amd64/etcd /tmp/etcd-$ETCD_VERSION-linux-amd64/etcdctl /usr/local/bin
