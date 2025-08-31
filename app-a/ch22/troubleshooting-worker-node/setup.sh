3ff8bd3bb23237429baaaed2415d3e811ac47ce2
Benjamin Muschko <benjamin.muschko@gmail.com>
2025-08-31 11:50:39 -0600

    More content

--- app-a/ch22/troubleshooting-worker-node/setup.sh
+++ app-a/ch22/troubleshooting-worker-node/setup.sh
@@ -1,3 +1,4 @@
-# On the control-plane node, corrupt the scheduler manifest:
-sudo mv /etc/kubernetes/manifests/kube-scheduler.yaml /etc/kubernetes/manifests/kube-scheduler.yaml.backup
-sudo bash -c 'cat /etc/kubernetes/manifests/kube-scheduler.yaml.backup | sed "s/image: registry.k8s.io\/kube-scheduler:.*/image: registry.k8s.io\/kube-scheduler:v1.99.0/" > /etc/kubernetes/manifests/kube-scheduler.yaml'
+# Simulate the problem
+# SSH into worker-node-2 and run:
+sudo systemctl stop kubelet
+kubectl cordon worker-node-2
